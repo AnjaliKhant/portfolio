7bc534454661cfecf0933cdcb81008682235ff6d
Anjali Khant <uks.bo.adi.tdg@gmail.com>
2019-04-15 15:22:02 +0000

    [Javascript Changes]

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -5,19 +5,23 @@
 	  vid.classList.add("stopfade");
 	}
 
-	vid.addEventListener('ended', function() {
-	// only functional if "loop" is removed 
-	vid.pause();
-	// to capture IE10
-	vidFade();
-	}); 
-	pauseButton.addEventListener("click", function() {
-	  vid.classList.toggle("stopfade");
-	  if (vid.paused) {
-	    vid.play();
-	    pauseButton.innerHTML = "<i class='icon-control-pause'></i>";
-	  } else {
-	    vid.pause();
-	    pauseButton.innerHTML = "<i class='icon-control-play'></i>";
-	  }
-	})
+	if(vid) {
+		vid.addEventListener('ended', function() {
+		// only functional if "loop" is removed 
+		vid.pause();
+		// to capture IE10
+		vidFade();
+		}); 	
+	}
+	if (pauseButton) {
+		pauseButton.addEventListener("click", function() {
+		  vid.classList.toggle("stopfade");
+		  if (vid.paused) {
+		    vid.play();
+		    pauseButton.innerHTML = "<i class='icon-control-pause'></i>";
+		  } else {
+		    vid.pause();
+		    pauseButton.innerHTML = "<i class='icon-control-play'></i>";
+		  }
+		})
+	}
