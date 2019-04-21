c768a6ab9dda10898625200dd2b90fbd54637c1e
Anjali Khant <uks.bo.adi.tdg@gmail.com>
2019-04-21 10:50:24 +0000

    Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    puts Rails.env
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Workspace
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.secret_key_base = '889aaa672187bd61b60d9c9583e627b04e7eb0f440cff29d50b0068e2d0f27b6cbd77f5acb3e96010f78116497c76a4405b6f13685cf305140c4a174aa2c82cf'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
